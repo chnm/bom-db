383169e06af0680e8f2b1fa21eba9f8f86cddd44
Jason Heppler <jason.heppler@gmail.com>
2021-09-15 14:23:44 -0500

    Sample data and basic styling

--- components/DataTable.vue
+++ components/DataTable.vue
@@ -9,7 +9,6 @@ the PostgreSQL API. -->
       :fixed-header="true"
         :search-options="{
         enabled: true,
-        trigger: 'enter'
         }"
         :pagination-options="{
             enabled: true
@@ -18,51 +17,37 @@ the PostgreSQL API. -->
 </template>
 <script>
 export default {
-  name: 'VueTable',
+  name: 'BoM',
   data(){
     return {
       columns: [
         {
-          label: 'Name',
-          field: 'name',
+          label: 'Parish',
+          field: 'parish',
         },
         {
-          label: 'Age',
-          field: 'age',
+          label: 'Burials',
+          field: 'buried',
           type: 'number',
         },
         {
-          label: 'Created On',
-          field: 'createdAt',
-          type: 'date',
-          dateInputFormat: 'yyyy-MM-dd',
-          dateOutputFormat: 'MMM do yy',
+          label: 'Plague',
+          field: 'plague',
+          type: 'number',
         },
         {
-          label: 'Percent',
-          field: 'score',
-          type: 'percentage',
+        label: 'Year',
+        field: 'year',
+        type: 'date',
+        dateInputFormat: 'yyyy',
+        dateOutputFormat: 'yyyy',
         },
+        {
+          label: 'Date span',
+          field: 'span',
+        }
       ],
-      rows: [
-        { id:1, name:"John", age: 20, createdAt: '',score: 0.03343 },
-        { id:2, name:"Jane", age: 24, createdAt: '2011-10-31', score: 0.03343 },
-        { id:3, name:"Susan", age: 16, createdAt: '2011-10-30', score: 0.03343 },
-        { id:4, name:"Chris", age: 55, createdAt: '2011-10-11', score: 0.03343 },
-        { id:5, name:"Dan", age: 40, createdAt: '2011-10-21', score: 0.03343 },
-        { id:6, name:"John", age: 20, createdAt: '2011-10-31', score: 0.03343 },
-        { id:6, name:"John", age: 20, createdAt: '2011-10-31', score: 0.03343 },
-        { id:6, name:"John", age: 20, createdAt: '2011-10-31', score: 0.03343 },
-        { id:6, name:"John", age: 20, createdAt: '2011-10-31', score: 0.03343 },
-        { id:6, name:"John", age: 20, createdAt: '2011-10-31', score: 0.03343 },
-        { id:6, name:"John", age: 20, createdAt: '2011-10-31', score: 0.03343 },
-        { id:6, name:"John", age: 20, createdAt: '2011-10-31', score: 0.03343 },
-        { id:6, name:"John", age: 20, createdAt: '2011-10-31', score: 0.03343 },
-        { id:6, name:"John", age: 20, createdAt: '2011-10-31', score: 0.03343 },
-        { id:6, name:"John", age: 20, createdAt: '2011-10-31', score: 0.03343 },
-        { id:6, name:"John", age: 20, createdAt: '2011-10-31', score: 0.03343 },
-        { id:6, name:"John", age: 20, createdAt: '2011-10-31', score: 0.03343 },
-      ],
+      rows: [{"parish":"St ALban Woodstreet","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"Alhallows Barking","buried":2,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"Alhallows Breadstreet","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"Alha••ows Great","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"Alhallows Honylane","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"Al••llows Lesse","buried":2,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"A••allows Lumbardstreet","buried":2,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"Alh•llows Staining","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"A••allows the Wall","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Alphage","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Andrew Hubbard","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Andrew Undershaft","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Andrew Wardrobe","buried":3,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Ann Aldersgate","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Ann Blackfryers","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Antholins Parish","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Austins Parish","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Bartholomew Exchange","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Bennet Fynck","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Bennet Gracechurch","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Bennet Paulswharf","buried":3,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Bennet Sherehog","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Botolph Billingsgate","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"Christs Church","buried":2,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Christophers","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Clement Eastcheap","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Dionis Backchurch","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Dunstan East","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Edmund Lumbardstr","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Ethelborough","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Faith","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Foster","buried":3,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Gabriel Fenchurch","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St George Borolphlane","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Gregory by St Pauls","buried":2,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Hellen","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St James Dukes place","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St James Garlickhithe","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St John Baptist","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St John Evangelist","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St John Zachary","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Katharine Coleman","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Katharine Crechurch","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Lawrence Jewry","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Lawrence Pountney","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Leonard Eastcheap","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Leonard Fosterlane","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Magnus Parish","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Margaret Lothbury","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Margaret Moses","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Margaret Newfishstreet","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Margaret Pattons","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Mary Abchurch","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Mary Aldermanbury","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Mary Aldermary","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Mary le Bow","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Mary Bothaw","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Mary Colechurch","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Mary Hill","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Mary Mounthaw","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Mary Sommerset","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Mary Stayning","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Mary Woolchurch","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Mary Woolnoth","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Martin Iremongerlane","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Martin Ludgate","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Martin Orgars","buried":2,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Martin Outwitch","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Martin Vintrey","buried":2,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Matthew Fridaystreet","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Maudlin Milkstreet","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Maudlin Oldfishstreet","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Michael Bassishaw","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Michael Cornhil","buried":2,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Michael Crookedlane","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Michael Queenhithe","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Michael Quern","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Michael Royal","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Michael Woodstreet","buried":2,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Mildred Breadstreet","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Mildred Poultrey","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Nicholas Acons","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Nicholas Coleabby","buried":3,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Nicholas Olaves","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Olave Hartstreet","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Olave Jewry","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Olave Silverstreet","buried":2,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Panc•as Soperlane","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Peter Cheap","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Peter Cornhil","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Peter Paulswharf","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Peter Poor","buried":2,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Steven Colemanstreet","buried":3,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Steven Walbrook","buried":1,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Swithin","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"St Thomas Apostle","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"},{"parish":"Trinity Parish","buried":0,"plague":0,"year":1664,"span":"1664-12-20--1664-12-27"}]
     };
   },
 };
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,7 @@
   <nav class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-indigo-500 mb-3">
     <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
       <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
-        <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" href="#pablo">
+        <a id="site-title" class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" href="#">
           London Bills of Mortality
         </a>
         <button class="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button" @click="toggleNavbar()">
@@ -11,6 +11,11 @@
       </div>
       <div :class="{'hidden': !showMenu, 'flex': showMenu}" class="lg:flex lg:flex-grow items-center">
         <ul class="flex flex-col lg:flex-row list-none ml-auto">
+          <li class="nav-item">
+            <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#">
+              <span class="ml-2">Blog</span>
+            </a>
+          </li>
           <li class="nav-item">
             <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#">
               <span class="ml-2">Database</span>
