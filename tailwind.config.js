f318916602860deed315d7babb6b7066a8de7569
Jason Heppler <jason.heppler@gmail.com>
2022-10-28 14:43:23 -0500

    Implement container sizes site-wide.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 module.exports = {
     content: ['./src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'],
     plugins: [
-      require('tw-elements/dist/plugin')
+      require('tw-elements/dist/plugin'),
     ],
-    theme: {   
-      extend: {     
+    theme: {
+      container: {
+        center: true,
+        screens: {
+          sm: '600px',
+          md: '728px',
+          lg: '984px',
+          xl: '1240px',
+          '2xl': '1496px',
+        },
+      },
+      extend: {   
         colors: {
           dbn: {
             green: '#3E3E32',
